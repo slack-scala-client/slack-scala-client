e72b0dfe9b1f44dda7ac55c257f0191ef035ae20
Bryan Gilbert <gilbertw1@gmail.com>
2015-04-22 14:15:13 -0400

    Implemented all channel based api endpoints

--- src/main/scala/slack/api/SlackApiClient.scala
+++ src/main/scala/slack/api/SlackApiClient.scala
@@ -32,11 +32,6 @@ class SlackApiClient(token: String) {
     res.map(_.as[AuthIdentity])
   }
 
-  def listChannels()(implicit ec: ExecutionContext): Future[Seq[Channel]] = {
-    val res = makeApiRequest("channels.list")
-    extract[Seq[Channel]](res, "channels")
-  }
-
   def archiveChannel(channelId: String)(implicit ec: ExecutionContext): Future[Boolean] = {
     val res = makeApiRequest("channels.archive", ("channel" -> channelId))
     extract[Boolean](res, "ok")
@@ -50,13 +45,71 @@ class SlackApiClient(token: String) {
   // TODO: Paging
   def channelHistory(channelId: String, latest: Option[Long] = None, oldest: Option[Long] = None,
       inclusive: Option[Int] = None, count: Option[Int] = None)(implicit ec: ExecutionContext): Future[HistoryChunk] = {
-    var params = Seq[(String,String)](("channel" -> channelId))
-    latest.foreach(l => params :+= ("latest" -> l.toString))
-    oldest.foreach(o => params :+= ("oldest" -> o.toString))
-    inclusive.foreach(i => params :+= ("inclusive" -> i.toString))
-    count.foreach(c => params :+= ("count" -> c.toString))
+    var params = createParams (
+      ("channel" -> channelId),
+      ("latest" -> latest),
+      ("oldest" -> oldest),
+      ("inclusive" -> inclusive),
+      ("count" -> count)
+    )
     val res = makeApiRequest("channels.history", params: _*)
     res.map(_.as[HistoryChunk])
+  }
+
+  def getChannelInfo(channelId: String)(implicit ec: ExecutionContext): Future[Channel] = {
+    val res = makeApiRequest("channels.info", ("channel" -> channelId))
+    extract[Channel](res, "channel")
+  }
+
+  def inviteToChannel(channelId: String, userId: String)(implicit ec: ExecutionContext): Future[Channel] = {
+    val res = makeApiRequest("channels.invite", ("channel" -> channelId), ("user" -> userId))
+    extract[Channel](res, "channel")
+  }
+
+  def joinChannel(channelId: String)(implicit ec: ExecutionContext): Future[Channel] = {
+    val res = makeApiRequest("channels.join", ("channel" -> channelId))
+    extract[Channel](res, "channel")
+  }
+
+  def kickFromChannel(channelId: String, userId: String)(implicit ec: ExecutionContext): Future[Boolean] = {
+    val res = makeApiRequest("channels.invite", ("channel" -> channelId), ("user" -> userId))
+    extract[Boolean](res, "ok")
+  }
+
+  def listChannels()(implicit ec: ExecutionContext): Future[Seq[Channel]] = {
+    val res = makeApiRequest("channels.list")
+    extract[Seq[Channel]](res, "channels")
+  }
+
+  def leaveChannel(channelId: String)(implicit ec: ExecutionContext): Future[Boolean] = {
+    val res = makeApiRequest("channels.leave", ("channel" -> channelId))
+    extract[Boolean](res, "ok")
+  }
+
+  def markChannel(channelId: String, ts: Long)(implicit ec: ExecutionContext): Future[Boolean] = {
+    val res = makeApiRequest("channels.mark", ("channel" -> channelId), ("ts" -> ts.toString))
+    extract[Boolean](res, "ok")
+  }
+
+  // TODO: Lite Channel Object
+  def renameChannel(channelId: String, name: String)(implicit ec: ExecutionContext): Future[Boolean] = {
+    val res = makeApiRequest("channels.rename", ("channel" -> channelId), ("name" -> name))
+    extract[Boolean](res, "ok")
+  }
+
+  def setChannelPurpose(channelId: String, purpose: String)(implicit ec: ExecutionContext): Future[String] = {
+    val res = makeApiRequest("channels.setPurpose", ("channel" -> channelId), ("purpose" -> purpose))
+    extract[String](res, "purpose")
+  }
+
+  def setChannelTopic(channelId: String, topic: String)(implicit ec: ExecutionContext): Future[String] = {
+    val res = makeApiRequest("channels.setTopic", ("channel" -> channelId), ("topic" -> topic))
+    extract[String](res, "topic")
+  }
+
+  def unarchiveChannel(channelId: String)(implicit ec: ExecutionContext): Future[Boolean] = {
+    val res = makeApiRequest("channels.unarchive", ("channel" -> channelId))
+    extract[Boolean](res, "ok")
   }
 
   private def makeApiRequest(apiMethod: String, queryParams: (String,String)*)(implicit ec: ExecutionContext): Future[JsValue] = {
@@ -81,6 +134,16 @@ class SlackApiClient(token: String) {
 
   private def extract[T](jsFuture: Future[JsValue], field: String)(implicit ec: ExecutionContext, fmt: Format[T]): Future[T] = {
     jsFuture.map(js => (js \ field).as[T])
+  }
+
+  private def createParams(params: (String,Any)*): Seq[(String,String)] = {
+    var paramList = Seq[(String,String)]()
+    params.foreach {
+      case (k, Some(v)) => paramList :+= (k -> v.toString)
+      case (k, None) => // Nothing - Filter out none
+      case (k, v) => paramList :+= (k -> v.toString)
+    }
+    paramList
   }
 }
 
