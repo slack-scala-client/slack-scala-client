7d26820cedac6e6e6df7ba1314bc85dd2e42d5c2
toidiu <toidiu@gmail.com>
2016-07-28 00:49:08 -0400

    add method to determine if the message was sent as a direct message or via a channel

--- src/main/scala/slack/SlackUtil.scala
+++ src/main/scala/slack/SlackUtil.scala
@@ -1,10 +1,16 @@
 package slack
 
+import slack.models.Message
+
 object SlackUtil {
 
   val mentionrx = """<@(\w+)>""".r
 
   def extractMentionedIds(text: String): Seq[String] = {
     mentionrx.findAllMatchIn(text).toVector.map(_.subgroups.head)
+  }
+
+  def isDirectMsg(m: Message): Boolean = {
+    m.channel.startsWith("D")
   }
 }
