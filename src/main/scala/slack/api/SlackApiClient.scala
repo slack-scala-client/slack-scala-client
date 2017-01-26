f50c57cb5130884e8552a33e34484e223908da1b
Andrew Catton <andrew@catton.ca>
2017-01-26 16:43:54 -0500

    Added thread_ts to postChatMessage

--- src/main/scala/slack/api/SlackApiClient.scala
+++ src/main/scala/slack/api/SlackApiClient.scala
@@ -192,7 +192,7 @@ class SlackApiClient(token: String) {
       parse: Option[String] = None, linkNames: Option[String] = None, attachments: Option[Seq[Attachment]] = None,
       unfurlLinks: Option[Boolean] = None, unfurlMedia: Option[Boolean] = None, iconUrl: Option[String] = None,
       iconEmoji: Option[String] = None, replaceOriginal: Option[Boolean]= None,
-      deleteOriginal: Option[Boolean] = None)(implicit ec: ExecutionContext): Future[String] = {
+      deleteOriginal: Option[Boolean] = None, threadTs: Option[String] = None)(implicit ec: ExecutionContext): Future[String] = {
     val res = makeApiMethodRequest (
       "chat.postMessage",
       "channel" -> channelId,
@@ -207,7 +207,9 @@ class SlackApiClient(token: String) {
       "icon_url" -> iconUrl,
       "icon_emoji" -> iconEmoji,
       "replace_original" -> replaceOriginal,
-      "delete_original" -> deleteOriginal)
+      "delete_original" -> deleteOriginal,
+      "thread_ts" -> threadTs
+    )
     extract[String](res, "ts")
   }
 
