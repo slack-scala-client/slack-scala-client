aa440c9cfc1f15ddd7a6a1242abac1ac94abedf3
Krisztian Lachata <krisztian.lachata@ovoenergy.com>
2018-12-02 17:28:23 +0000

    address Issue #67

--- src/main/scala/slack/api/BlockingSlackApiClient.scala
+++ src/main/scala/slack/api/BlockingSlackApiClient.scala
@@ -3,27 +3,29 @@ package slack.api
 import slack.models._
 
 import java.io.File
+
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.Uri
 import play.api.libs.json._
+import slack.api.SlackApiClient.defaultSlackApiBaseUri
 
 object BlockingSlackApiClient {
 
-  def apply(token: String, duration: FiniteDuration = 5.seconds): BlockingSlackApiClient = {
-    new BlockingSlackApiClient(token, duration)
+  def apply(token: String, slackApiBaseUri:Uri = SlackApiClient.defaultSlackApiBaseUri, duration: FiniteDuration = 5.seconds): BlockingSlackApiClient = {
+    new BlockingSlackApiClient(token, slackApiBaseUri, duration)
   }
 
   def exchangeOauthForToken(clientId: String, clientSecret: String, code: String, redirectUri: Option[String] = None,
-      duration: FiniteDuration = 5.seconds)(implicit system: ActorSystem): AccessToken = {
-    Await.result(SlackApiClient.exchangeOauthForToken(clientId, clientSecret, code, redirectUri), duration)
+      duration: FiniteDuration = 5.seconds, slackApiBaseUri:Uri = defaultSlackApiBaseUri)(implicit system: ActorSystem): AccessToken = {
+    Await.result(SlackApiClient.exchangeOauthForToken(clientId, clientSecret, code, redirectUri, slackApiBaseUri), duration)
   }
 }
 
-class BlockingSlackApiClient(token: String, duration: FiniteDuration = 5.seconds) {
-  val client = new SlackApiClient(token)
-
+class BlockingSlackApiClient private(token: String, slackApiBaseUri:Uri, duration: FiniteDuration) {
+  val client = SlackApiClient(token, slackApiBaseUri)
 
   /**************************/
   /***   Test Endpoints   ***/
