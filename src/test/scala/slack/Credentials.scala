350ac11a6ee9cbede5caab716640ff21df67f1fe
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2020-02-25 22:47:20 +0100

    Skipping tests if credentials don't exist

--- src/test/scala/slack/Credentials.scala
+++ src/test/scala/slack/Credentials.scala
@@ -2,11 +2,13 @@ package slack
 
 import akka.actor.ActorSystem
 
+import scala.util.Try
+
 trait Credentials {
 
   implicit val system = ActorSystem("slack")
-  val user = system.settings.config.getString("test.userId")
-  val token = sys.env.getOrElse("SLACK_API", system.settings.config.getString("test.apiKey"))
+  val user: Option[String] = Try(system.settings.config.getString("test.userId")).toOption
+  val token: Option[String] = Try(sys.env.getOrElse("SLACK_API", system.settings.config.getString("test.apiKey"))).toOption
   val rtmToken = token
 
 }
--- src/test/scala/slack/SlackRtmClientTest.scala
+++ src/test/scala/slack/SlackRtmClientTest.scala
@@ -12,43 +12,50 @@ import scala.concurrent.{Await, Promise}
 
 class SlackRtmClientTest extends FunSuite with Matchers with Credentials {
 
-  val channel = system.settings.config.getString("test.channel")
+  rtmToken match {
+    case Some(slackToken) =>
 
-  lazy val rtmClient = {
-    val rtm = SlackRtmClient(rtmToken)
-    assert(rtm.state.self.id != null)
-    rtm
-  }
-  test("rtm typing") {
-    rtmClient.indicateTyping(channel)
-  }
+      val channel = system.settings.config.getString("test.channel")
 
-  test("team domain") {
-    val domain = rtmClient.state.team.domain
-    val name = rtmClient.state.team.name
-    domain should be(system.settings.config.getString("test.team.domain"))
-    name should be(system.settings.config.getString("test.team.name"))
-  }
+      lazy val rtmClient = {
+        val rtm = SlackRtmClient(slackToken)
+        assert(rtm.state.self.id != null)
+        rtm
+      }
+      test("rtm typing") {
+        rtmClient.indicateTyping(channel)
+      }
 
-  test("send message and parse reply") {
-    val latch = new CountDownLatch(1)
-    val promise = Promise[Long]()
-    rtmClient.onEvent {
-      case r: Reply =>
-        assert(r.reply_to.equals(Await.result(promise.future, 2.seconds)))
-        latch.countDown()
-      case e => println("EVENT >>>>> " + e)
-    }
-    val messageIdFuture = rtmClient.sendMessage(channel, "Hi there")
-    promise.completeWith(messageIdFuture)
-    latch.await(5, TimeUnit.SECONDS)
-  }
+      test("team domain") {
+        val domain = rtmClient.state.team.domain
+        val name = rtmClient.state.team.name
+        domain should be(system.settings.config.getString("test.team.domain"))
+        name should be(system.settings.config.getString("test.team.name"))
+      }
 
-  ignore("edit message as bot") {
-    val rtmApi = SlackApiClient(rtmToken)
-    val future = rtmApi.updateChatMessage(channel, "1465891701.000006", "edit-x", asUser = Some(true))
-    val result = Await.result(future, 5.seconds)
-    assert(result.ok.equals(true))
-  }
+      test("send message and parse reply") {
+        val latch = new CountDownLatch(1)
+        val promise = Promise[Long]()
+        rtmClient.onEvent {
+          case r: Reply =>
+            assert(r.reply_to.equals(Await.result(promise.future, 2.seconds)))
+            latch.countDown()
+          case e => println("EVENT >>>>> " + e)
+        }
+        val messageIdFuture = rtmClient.sendMessage(channel, "Hi there")
+        promise.completeWith(messageIdFuture)
+        latch.await(5, TimeUnit.SECONDS)
+      }
 
+      ignore("edit message as bot") {
+        val rtmApi = SlackApiClient(slackToken)
+        val future = rtmApi.updateChatMessage(channel, "1465891701.000006", "edit-x", asUser = Some(true))
+        val result = Await.result(future, 5.seconds)
+        assert(result.ok.equals(true))
+      }
+
+    case _ =>
+      println("Skipping the test as the API credentials are not available")
+
+  }
 }
