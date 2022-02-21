00f91ca3a25b7b297c3e470154102df341428481
Scala Steward <me@scala-steward.org>
2022-02-21 10:37:15 +0100

    Update akka-actor, akka-stream to 2.6.18

--- project/Build.scala
+++ project/Build.scala
@@ -46,7 +46,7 @@ object BuildSettings {
 }
 
 object Dependencies {
-  val akkaVersion = "2.5.32"
+  val akkaVersion = "2.6.18"
 
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
   val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
