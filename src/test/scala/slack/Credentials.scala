46b99ce352022ca518ac599ff454ae9b0dcb6fca
Greg Oledzki <goledzki@sumologic.com>
2023-06-20 12:11:47 +0200

    Refactoring - adding explicit type declarations for implicits

--- src/test/scala/slack/Credentials.scala
+++ src/test/scala/slack/Credentials.scala
@@ -6,7 +6,7 @@ import scala.util.Try
 
 trait Credentials {
 
-  implicit val system = ActorSystem("slack")
+  implicit val system: ActorSystem = ActorSystem("slack")
   val user: Option[String] = Try(system.settings.config.getString("test.userId")).toOption
   val token: Option[String] = Try(sys.env.getOrElse("SLACK_API", system.settings.config.getString("test.apiKey"))).toOption
   val rtmToken = token
