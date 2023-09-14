5462994b87dd7f0e759c80c740715d3eaa452379
Scala Steward <scala_steward@virtuslab.com>
2023-09-14 03:14:38 +0000

    Update scala-library to 2.13.12

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object BuildSettings {
     organization       := buildOrganization,
     version            := buildVersion,
     scalaVersion       := buildScalaVersion,
-    crossScalaVersions :=  Seq(scalaVersion.value, "2.13.11"),
+    crossScalaVersions :=  Seq(scalaVersion.value, "2.13.12"),
     publishMavenStyle  := true,
     credentials += Credentials(Path.userHome / ".sbt" / ".credentials"),
     publishTo          := {
