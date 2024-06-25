9ee6565d4ded407fd08c1942547d644bed83fb8f
Scala Steward <scala_steward@virtuslab.com>
2024-06-25 03:27:47 +0000

    Update pekko-actor, pekko-stream to 1.0.3

--- project/Build.scala
+++ project/Build.scala
@@ -46,7 +46,7 @@ object BuildSettings {
 }
 
 object Dependencies {
-  val pekkoVersion = "1.0.2"
+  val pekkoVersion = "1.0.3"
 
   val pekkoActor = "org.apache.pekko" %% "pekko-actor" % pekkoVersion
   val pekkoStream = "org.apache.pekko" %% "pekko-stream" % pekkoVersion
